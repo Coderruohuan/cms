c6162a33571b406c762ee64806d4edcd06655eb3
wwn <1204980311@qq.com>
2018-04-17 18:41:51 +0800

    bean 初始化验证

--- src/main/java/com/wang/ssmTest/BeanClass.java
+++ src/main/java/com/wang/ssmTest/BeanClass.java
@@ -2,8 +2,14 @@ package com.wang.ssmTest;
 
 public class BeanClass {
 
+	private Integer count = 9;
+
 	public BeanClass() {
 		System.out.println("bean 是否已经初始化了.....");
+	}
+
+	public void print() {
+		System.out.println("print.....");
 	}
 
 }
