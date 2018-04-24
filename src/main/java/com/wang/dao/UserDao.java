8cc45304987ae0311afd1136c5c3562c2578e0af
wwn <1204980311@qq.com>
2018-04-24 18:22:04 +0800

    dao

--- src/main/java/com/wang/dao/UserDao.java
+++ src/main/java/com/wang/dao/UserDao.java
@@ -1,9 +1,11 @@
 package com.wang.dao;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.wang.model.User;
 
 public interface UserDao {
 
-	int insert(User user);
+	int insert(@Param("user") User user);
 
 }
--- src/main/java/com/wang/model/User.java
+++ src/main/java/com/wang/model/User.java
@@ -1,5 +1,8 @@
 package com.wang.model;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class User {
 
 	private String username;
